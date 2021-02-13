f46ffc198f3fe9d7d5ff445d000cf08011f92db8
Sangeeta <saangeeeeta@gmail.com>
2021-02-13 21:19:50 +0530

    Pushing the final thing on heroku

--- routes/tourContent.js
+++ routes/tourContent.js
@@ -12,13 +12,13 @@ const data = [{
     },
     {
         id: 'Gate-Way-Of-India',
-        src: '/photos/Tour/Gatewayofindia.jpg',
+        src: '/photos/Background/bg8.jpg',
         name: 'Gateway of India',
         info: 'This iconic structure is known as the Gateway of India and was built to honor the visit of King George V and Queen Mary to Bombay (now Mumbai) in the year 1911. Surprisingly, the development of the structure was finished in the year 1924 and since then it has been one of the most visited vacation spots in Mumbai. You can grab some fabulous photography shots at this spot, lip-smacking street foods like bhelpuri, Sev puri, and many more. With the walled limit of the structure brushing with the Arabian Sea, you will notice a vast number of tourists enjoying the spectacular view in midst of all the hustle and bustle of this metropolis.'
     },
     {
         id: 'Taj-Hotel',
-        src: '/photos/Tour/tajHotel.jpg',
+        src: '/photos/Tour/taj-hotel.jpg',
         name: 'Taj Mahal hotel',
         info: 'The Taj Mahal Palace Hotel is a five-star, lavish hotel built in the Saracenic Revival style in the Colaba region of Mumbai and is just a stone throw away from the Gateway of India. Also known as the "Taj Mahal Hotel" or "the Taj". The hotel is viewed as probably the best in the East since the reign of the British Raj. '
     },
@@ -34,6 +34,25 @@ const data = [{
         name: 'Dhobi Talao',
         info: 'Dhobi Talao boasts of having the worlds largest outdoor laundry which is also known as Dhobi Ghat. Formed back in 1890, the renowned laundromat is tremendously adored by photographers who come to take vivid pictures of washermen toiling hard to clean the huge pile of clothes. Strings of beautifully coloured garments drying under the Mumbai sun additionally make for a spectacular view.'
     },
+    {
+        id: 'Marine-Drive',
+        src: '/photos/Tour/marine.jpg',
+        name: 'Marine Drive',
+        info: 'Marine Drive is the most easily identifiable landmark associated with Mumbai and is indicative of the glamor and glitter of the city. It is essentially 3.6 km long, arc-shaped boulevard along the South Mumbai coast that starts at the southern end of Nariman Point and ends at Girgaum Chowpatty, popularly known as Chowpatty Beach.'
+    },
+    {
+        id: 'Juhu-Beach',
+        src: '/photos/Tour/Juhubeach.jpg',
+        name: 'Juhu Beach',
+        info: 'The Juhu Beach in Mumbai is one of the most famous Indian beaches. This beach is known to be the one of the most-visited beaches of India and is situated in "Ville Parle". It is located approximately 18 km from the city center and is a desired location for most film shootings.'
+    },
+    {
+        id: 'Kanheri-Caves',
+        src: '/photos/Tour/kanheriCaves.jpg',
+        name: 'Kanheri Caves',
+        info: 'Nestled in the Sanjay Gandhi National Park at Borivali, the Kanheri Caves are also known as the "lungs of Mumbai", because this is the only place in the city with the maximum amount of greenery and consequently, a lot of fresh air. The caves date back to 1st century BC and are believed to be one of the oldest cave formations of the country.'
+    },
 ]
 
 module.exports = data;
+
